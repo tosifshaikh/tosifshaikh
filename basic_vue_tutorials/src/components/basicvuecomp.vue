5b786a0d6e8434221cfa894e616e3fff6fc295bd
Tosif <shekh.tosif@gmail.com>
2021-10-23 22:42:20 -0700

    vue tutorial

---
+++ basic_vue_tutorials/src/components/basicvuecomp.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Inside the component</h1>
+    <h1>{{ data }}</h1>
+    <h1>{{ msg }}</h1>
+    <button v-on:click="hello" >Call hello function</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "basicvuecomp",
+  props : {
+    data : String,
+    msg : String,
+  },
+  methods :{
+    hello() {
+      console.log('hello world');
+    }
+  }
+}
+</script>
