cc268764ee8aaff0e88aac11d17214a1e69763d9
Tosif <shekh.tosif@gmail.com>
2021-11-10 18:21:29 +0530

    laravel-vue-10-11-21

---
+++ laravel-vue/resources/js/App.vue
@@ -0,0 +1,30 @@
+<template>
+<main>
+    <Header />
+    <div id="layoutSidenav">
+        <Sidebar />
+        <div id="layoutSidenav_content">
+            <router-view></router-view>
+            <Footer />
+        </div>
+    </div>
+</main>
+</template>
+
+<script>
+import Header from './components/Header'
+import Sidebar from './components/Sidebar'
+import Footer from './components/Footer'
+export default {
+    name: "App",
+    components :{
+        Header,
+        Sidebar,
+        Footer
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ laravel-vue/resources/js/views/Categories.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="container-fluid px-4">
+        <ol class="breadcrumb mt-4">
+            <li class="breadcrumb-item active">
+                <router-link to="/">
+                    Dashboard
+                </router-link>
+            </li>
+
+            <li class="breadcrumb-item">
+                Categories
+            </li>
+        </ol>
+      <div class="card mb-3">
+          <div class="card-header">
+              <i class="fas fa-chart-area"></i>
+              Categories Management
+          </div>
+            <div class="card-body">
+               <table class="table">
+                <thead>
+                    <tr>
+                        <td>ID</td>
+                        <td>Name</td>
+                        <td>Image</td>
+                        <td>Action</td>
+                    </tr>
+                </thead>
+                   <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>ABC</td>
+                        <td>Image</td>
+                        <td>
+                            <button class="btn btn-primary btn-sm"><span class="fa fa-edit"></span></button>
+                            <button class="btn btn-danger btn-sm"><span class="fa fa-trash"></span></button>
+                        </td>
+                    </tr>
+                   </tbody>
+               </table>
+            </div>
+      </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Categories"
+}
+</script>
+
+<style scoped>
+
+</style>
