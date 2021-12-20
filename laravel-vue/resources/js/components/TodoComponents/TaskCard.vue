dde3524975d2cb73488c04ee8f73ba1702b7fa9b
Tosif <shekh.tosif@gmail.com>
2021-12-20 18:14:55 +0530

    todo list-20-12-21

--- laravel-vue/resources/js/components/TodoComponents/TaskCard.vue
+++ laravel-vue/resources/js/components/TodoComponents/TaskCard.vue
@@ -1,18 +1,30 @@
 <template>
     <div class="bg-white shadow rounded px-3 pt-3 pb-5 border border-white">
-        <div class="flex justify-between">
-            <p class="text-gray-700 font-semibold font-sans tracking-wide text-sm">{{task.task_title}}</p>
+        <div class="flex align-top float-right">
+<!--            <button class="float-xl-right" ><span class="fas fa-trash-alt" ></span></button>-->
+            <span class="text-sm text-gray-600">{{formatDate(task.updated_at)}}</span>
+            </div>
+        <div class="card-title">
+            <badge v-if="task.priority" :color="badgeColor"></badge>
+            <p class="text-gray-700 font-semibold font-sans tracking-wide text-sm">{{task.title}}</p>
+        </div>
+        <div class="flex justify-content-lg-end">
 
+
+
+
+
+        </div>
+
+        <div class="flex mt-4 justify-between items-start-l">
             <img
                     class="w-6 h-6 rounded-full ml-3"
                     src="https://pickaface.net/gallery/avatar/unr_sample_161118_2054_ynlrg.png"
                     alt="Avatar"
             >
+
         </div>
-        <div class="flex mt-4 justify-between items-center">
-            <span class="text-sm text-gray-600">{{formatDate(task.updated_at)}}</span>
-            <badge v-if="task.priority" :color="badgeColor"></badge>
-        </div>
+
     </div>
 </template>
 <script>
