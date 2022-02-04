cdca8007530be928bf6954b430ae771000eb8b03
Tosif <shekh.tosif@gmail.com>
2022-02-04 13:37:15 +0530

    node basic

---
+++ node/Node.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var dt = require('./myfirstmodule');
+
+/*http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("The date and time are currently: " + dt.myDateTime());
+    res.end();
+}).listen(8080);*/
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Hello World!');
+    res.write("The date and time are currently: " + dt.myDateTime());
+    res.end();
+}).listen(8080);
