f92e074d10d99d3c801c4a80d1329785daa75441
Tosif <shekh.tosif@gmail.com>
2022-04-04 18:18:11 +0530

    persistance storage

--- laravel_vue_blog/resources/js/store/store.js
+++ laravel_vue_blog/resources/js/store/store.js
@@ -1,31 +1,44 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import VuexPersistence from 'vuex-persist';
+import SecureLS from 'secure-ls';
+const ls = new SecureLS({isCompression :false});
 import auth from './modules/auth/index';
 import { LOADING_SPINNER_SHOW_MUTATION } from './storeconstants';
 Vue.use(Vuex);
 const vuexPersist = new VuexPersistence({
-    asyncStorage: true,
-    key: "vuexPersistStorage_default",
-    storage: window.localStorage,
-    saveState: async (key, state, storage) => {
-        let data = state;
+  //  asyncStorage: true,
+    //key: "vuexPersistStorage_default",
+    paths: ['auth'],
+    //storage: window.localStorage,
+   //supportCircular: true,
+    storage: {
+         getItem: (key) => ls.get(key),
+        setItem: (key, value) => {
+            console.log(key, value,'log');
+            ls.set(key, value)
+        },
+         removeItem: (key) => ls.remove(key)
+    },
+    //  saveState: async (key, state, storage) => {
+    //     let data = state;
 
-        if (storage && data) {
-        }
-        console.log(key, data)
-        storage.setItem(key, data);
-      },
-      restoreState: async function (key, storage) {
-        let data = await storage.getItem(key);
-        if (await data) {
-          try {
-          } catch (e) {
-            console.log(e);
-          }
-        }
-        return await data;
-      }
+    //     if (storage && state) {
+    //     }
+
+    //     console.log(key, JSON.stringify(ls.data),ls.set('jjjjjj','55555'))
+    //     storage.setItem(key,data.auth);
+    //   },
+    //   restoreState: async function (key, storage) {
+    //     let data = await storage.getItem(key);
+    //     if (await data) {
+    //       try {
+    //       } catch (e) {
+    //         console.log(e);
+    //       }
+    //     }
+    //     return await data;
+    //   }
   })
 export default new Vuex.Store({
     plugins: [vuexPersist.plugin],
