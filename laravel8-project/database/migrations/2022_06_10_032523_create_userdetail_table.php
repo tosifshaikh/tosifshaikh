ab189215f917faa59c9f3f7bd00191fc1b906762
Tosif <shekh.tosif@gmail.com>
2022-06-10 18:32:53 +0530

    laravel-8 and jquery-10-6-2022

---
+++ laravel8-project/database/migrations/2022_06_10_032523_create_userdetail_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserdetailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->id()->unsigned();
+            $table->string('name');
+            $table->string('email');
+            $table->tinyInteger('gender')->default(0);
+            $table->string('file')->nullable();
+            $table->date('bdate')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_details');
+    }
+}
---
+++ laravel8-project/database/migrations/2022_06_10_064258_create_hobbies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHobbiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hobbies', function (Blueprint $table) {
+            $table->id()->unsigned();
+            $table->bigInteger('userDetail_id')->unsigned();
+            $table->string('hobby_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hobbies');
+    }
+}
