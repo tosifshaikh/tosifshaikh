633f8e12c337b018825c670644e249f91099feba
Tosif <shekh.tosif@gmail.com>
2023-05-11 17:03:00 +0530

    Tailwindcss responsive mircosoft clone

---
+++ tailwind-responsive-microsoft-clone/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      scale: {
+        '-100': '-1',
+    }
+    },
+  },
+  plugins: [],
+}
+
